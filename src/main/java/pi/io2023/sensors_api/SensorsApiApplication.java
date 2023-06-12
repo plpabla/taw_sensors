217ebf61de701d9cef1a1df89b1ee5d3acd496ac
pawel-laptop <p.blaszczy@gmail.com>
2023-06-12 21:03:09 +0200

    initial commit
    Spring application with Maven and initial dependencies:
    - lombok
    - spring web

---
+++ src/main/java/pi/io2023/sensors_api/SensorsApiApplication.java
@@ -0,0 +1,13 @@
+package pi.io2023.sensors_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pi/io2023/sensors_api/SensorsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pi.io2023.sensors_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
