f0dc7b90cff31309da7338d35fa46bcae0024224
pawel-laptop <p.blaszczy@gmail.com>
2023-06-19 20:32:23 +0200

    add ping with corresponding test

--- src/main/java/pi/io2023/sensors_api/Api.java
+++ src/main/java/pi/io2023/sensors_api/Api.java
@@ -1,4 +1,17 @@
 package pi.io2023.sensors_api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api")
 public class Api {
+    @Autowired
+    private ArrayDB database;
+
+    @GetMapping("/ping")
+    public String ping()
+    {
+        return "pong.";
+    }
 }
--- src/test/java/pi/io2023/sensors_api/ApiTest.java
+++ src/test/java/pi/io2023/sensors_api/ApiTest.java
@@ -1,7 +1,29 @@
 package pi.io2023.sensors_api;
 
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class ApiTest {
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    void pingWorks() throws Exception
+    {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/ping"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("pong."));
+    }
 }
