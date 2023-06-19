d55ec681c52c33c93cb0ee8c1e607162d0a8978c
pawel-laptop <p.blaszczy@gmail.com>
2023-06-19 20:10:11 +0200

    delete() functionality, getTable()

--- src/main/java/pi/io2023/sensors_api/DBInterface.java
+++ src/main/java/pi/io2023/sensors_api/DBInterface.java
@@ -1,8 +1,11 @@
 package pi.io2023.sensors_api;
 
+import java.util.List;
+
 public interface DBInterface {
     String insert(DTO obj);
-    String delete(DTO obj);
+    public List<DTO> getTable(String tableName);
+    void delete(String tableName, String id);
 
-    public DTO getGivenId(String tableName, String id);
+    DTO getGivenId(String tableName, String id);
 }
